fc2b0cf258fdbe08f884969061f85c8bc77ff166
Zac Johnson <zacjohnson333@gmail.com>
2021-11-03 10:39:34 -0500

    Serve static assests with public folder

--- index.js
+++ index.js
@@ -34,6 +34,8 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 app.use('/', home);
