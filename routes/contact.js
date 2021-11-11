a7378a7dcef4901e2252c31259d1b909eebc9452
Zac Johnson <zacjohnson333@gmail.com>
2021-11-10 19:02:08 -0600

    Add Bootstrap client-side validation. Add Joi schema server-side validation including joi & joi-phone-number packages. Add Joi validation middleware.

--- routes/contact.js
+++ routes/contact.js
@@ -4,7 +4,11 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../utilities/catchAsync');
 const ejsMate = require('ejs-mate');    // an engine that makes sense of ejs
+const Joi = require('joi');
+const joiPhoneNumber = Joi.extend(require('joi-phone-number'));
+const validateContactForm = require('../utilities/validateContactForm');
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 const SMTPTransport = require('nodemailer/lib/smtp-transport');
@@ -12,16 +16,14 @@ const GMAIL_USER = process.env.GMAIL_USER;
 const GMAIL_PASS = process.env.GMAIL_PASS;
 
 
-const OAuth2 = google.auth.OAuth2;
+const OAuth2 = google.auth.OAuth2;  // I think I can delete this? try later
 
 
 router.get('/', async (req, res) => {       // need to make a post route for contact? Not sure where to send it yet...
     res.render('pages/contact');
 });
 
-router.post('/', async (req, res) => {
-
-
+router.post('/', validateContactForm, catchAsync(async (req, res) => {
     // Instantiate the SMTP servier
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
@@ -51,7 +53,7 @@ router.post('/', async (req, res) => {
             res.render('pages/contact-success')
         }
     })
-});
+}));
 
 
 module.exports = router;
---
+++ utilities/validateContactForm.js
@@ -0,0 +1,24 @@
+const Joi = require('joi');
+const joiPhoneNumber = Joi.extend(require('joi-phone-number'));
+const ExpressError = require('./ExpressError');
+
+module.exports = (req, res, next) => {
+    const contactSchema = Joi.object({
+        contact: Joi.object({
+            name: Joi.string().required(),
+            number: joiPhoneNumber.string().phoneNumber().required(),
+            email: Joi.string().email().required(),
+            message: Joi.string().required()
+        }).required()
+    })
+    const { error } = contactSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
+// In the project, we put the schema in it's own file at the top level
+// I'm skipping that as this is the only schema we need
