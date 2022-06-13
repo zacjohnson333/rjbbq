6e2e7ad5e47b91f4fad42c62c65339d54b24c543
Zac Johnson <zacjohnson333@gmail.com>
2022-06-13 14:52:13 -0500

    Replace smoker image on Catering page. Change map on Next pagefrom light theme to streets theme. Change copyright on footer from 2021 to     2022. Change text on Story page. Change text on Catering page.

--- public/javascripts/nextPageMap.js
+++ public/javascripts/nextPageMap.js
@@ -14,7 +14,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
+    style: 'mapbox://styles/mapbox/streets-v11',
     center: coordinates,
     zoom: 9,
 });
